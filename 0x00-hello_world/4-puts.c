8c3338733bc9034a37fecd59255524a5d1782070
wabwi <wabwiibrahim@gmail.com>
2022-09-12 21:22:55 -0700

    updated 4-puts.c to betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-
-int main(void) {
+/**
+ *
+ *main - Uses puts function to print a line
+ *
+ *Description: We are unable to use printf so must use put to print this string
+ *Return: Code should return a 0
+ *
+ */
+int main(void) 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
