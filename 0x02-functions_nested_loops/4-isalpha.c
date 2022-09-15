8c03d5c70901df5c3f298ddd8ca1d7d5fd384dd5
wabwi <wabwiibrahim@gmail.com>
2022-09-14 19:56:01 -0700

    updated 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -13,16 +13,14 @@ int _isalpha(int c)
 	char lower, upper;
 
 	int isletter = 0;
-
-
+	
 	for (lower = 'a'; lower <= 'z'; lower++)
 	{
-			for (upper = 'A'; upper <= 'Z'; upper++)
-			{
-
-				if (c == lower || c == upper)
-					isletter = 1;
-			}
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (c == lower || c == upper)
+				isletter = 1;
+		}
 
 	}
 
