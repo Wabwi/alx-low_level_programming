97bddd8ccc014a99d063e1b5e351cf6c5b7cfdfb
wabwi <wabwiibrahim@gmail.com>
2022-09-14 23:08:47 -0700

    updated 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int i;
 
-	long int fibonacci[98];
+	unsigned long int fibonacci[98];
 
 	fibonacci[0] = 1;
 
 	fibonacci[1] = 2;
 
-	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
+	printf("%lu, %lu, ", fibonacci[0], fibonacci[1]);
 
 
 
@@ -26,10 +26,10 @@ int main(void)
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 
 		if (i == 98)
-			printf("%ld\n", fibonacci[i]);
+			printf("%lu\n", fibonacci[i]);
 
 		else
-			printf("%ld, ", fibonacci[i]);
+			printf("%lu, ", fibonacci[i]);
 
 	}
 
