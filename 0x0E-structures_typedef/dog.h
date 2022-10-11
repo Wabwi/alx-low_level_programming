6bf9627ab183523cae6866ed4c5d024521ec1e41
wabwi <wabwiibrahim@gmail.com>
2022-10-10 23:20:00 -0700

    updated dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
-#ifndef DOG
-#define DOG
+#ifndef _DOG_H
+#define _DOG_H
 
+/**
+ * struct dog - describes the dog
+ * @name: name of dog
+ * @age: how old is that dog?
+ * @owner: who owns that dog?
+ */
 struct dog
 {
 	char *name;
