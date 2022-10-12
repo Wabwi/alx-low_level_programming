137cc1112baa821b621cda7b8a93b52f13031bf3
wabwi <wabwiibrahim@gmail.com>
2022-10-11 19:58:37 -0700

    updated 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,7 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
-#define ABS(x) (x >= 0 ? (x) : (-x))
+#ifndef _FUNC_LIKE_MACRO_H
+
+#define _FUNC_LIKE_MACRO_H
+
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+
 #endif
